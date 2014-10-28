1dee313ec194bb120008576a23f5b11aa7937298
RobertoCastillo <rcastillo1031@gmail.com>
2014-10-27 23:09:54 -0400

    Add files to resumeBuilder repo

---
+++ js/resumeBuilder.js
@@ -0,0 +1,5 @@
+var awesomeThoughts = "I am Roberto Castillo and I am AWESOME!";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
+console.log(funThoughts);
