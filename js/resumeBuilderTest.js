2f995bacf03e40ee0dff9998b3984d789164caa3
RobertoCastillo <rcastillo1031@gmail.com>
2014-11-07 22:05:32 -0500

    Add resumeBuilderTest

---
+++ js/resumeBuilderTest.js
@@ -0,0 +1,62 @@
+var work = {
+	"jobs" : [
+		{
+			"employer" : "AT&T",
+			"title" : "Community Manager",
+			"location" : "Paramus, NJ, US",
+			"dates" : "March 2008 - Present",
+			"description" : "Designed, maintain and moderate the AT&T Business Community"
+		},
+		{
+			"employer" : "AT&T",
+			"title" : "Team Manager",
+			"location" : "Paramus, NJ, US",
+			"dates" : "January 2002 - March 2008",
+			"description" : "Managed and developed Tier I and II employees in several customer service departments"
+		}
+	]
+}
+
+var projects = {
+	"projects" : {
+		"title" : "AT&T Business Community",
+		"dates" : "March 2011 - Present",
+		"description" : "The AT&T Business Community is a public forum where professionals can ask questions to other members or Community staff about any AT&T products or services"
+	}
+}
+
+var bio = {
+	"name" : "Roberto Castillo",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "973-420-4278",
+		"email" : "rcastillo1031@gmail.com",
+		"github" : "RobertoCastillo"
+	},
+	"welcomeMessage" : "lorem ipsum dolor sit amet etc etc etc.",
+	"skills" : ["Project Management", "Customer Service", "HTML", "CSS", "Javascript", "Computer Hardware"],
+}
+
+var education = {
+	"schools" : {
+		"name" : "Dover Business College",
+		"city" : "Paramus, NJ, US",
+		"degree" : "Certificate",
+		"major" : "Networking",
+		"graduationYear" : 2002
+	},
+	"onlineCourses" : [
+	{
+		"name" : "ONLC Training Centers",
+		"degree" : "Certificate",
+		"major" : "Web Design",
+		"graduationYear" : 2014
+	},
+	{
+		"name" : "Udacity",
+		"degree" : "Nanodegree",
+		"major" : "Front-End Web Developer",
+		"graduationYear" : 2014
+	}
+	]
+}
